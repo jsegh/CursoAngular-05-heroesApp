9cd2ea2152e3cc4b9a0bee75168f68cf0c0f20f6
javSanz <jsegh00@gmail.com>
2022-11-02 10:39:39 +0100

    aver

--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth.service';
+import { Auth } from '../../../auth/interfaces/auth.interfaces';
 
 @Component({
   selector: 'app-home',
@@ -11,9 +14,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  get auth() {
+    return this.authService.auth;
+  }
+
+  constructor(private router:Router,
+              private authService:AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  logout(){
+    //ir al backend, confirmar usuario-pwd
+
+    //usuario, almacenado en un servicio, disponible en toda la app
+    localStorage.removeItem('token');
+    this.router.navigate(['./auth']);
   }
 
 }
